6c65048834c98e85f6a861ad5714948e0922f1f1
ddutov <165052103+ddutov@users.noreply.github.com>
2024-06-20 00:59:15 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""Напишите программу, которая создает два потока.
+Первый поток должен выводить числа от 1 до 10 с интервалом в 1 секунду.
+Второй поток должен выводить буквы от 'a' до 'j' с тем же интервалом.
+Оба потока должны работать параллельно."""
+
+import time
+from threading import Thread
+
+
+def printing(var):
+    for i in range(len(var)):
+        print(var[i])
+        time.sleep(1)
+
+
+first_var = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+second_var = tuple('abcdefghij')
+thread = Thread(target=printing, args=(first_var, ))
+thread.start()
+time.sleep(0.001)
+printing(second_var, )
+thread.join()
